f73ee3aef8caa397465ddc938e836dd702a7bf15
Aditya Raj Bhagat <adityabhagat900@gmail.com>
2022-08-22 02:10:33 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/go-webserver
+
+go 1.17
--- main.go
+++ main.go
@@ -7,15 +7,15 @@ import(
 )
 
 func formHandler(w http.ResponseWriter ,r *http.Request){
-	if err := r.ParseForm(); err !=nil{
+	if err := r.ParseForm(); err != nil{
 		fmt.Fprintf(w,"ParseForm() err : %v",err)
 		return
 	}
 	fmt.Fprintf(w,"POST request Sucessful\n")
 	name :=r.FormValue("name")
 	address := r.FormValue("address")
-	fmt.Fprintf(w,"Name=%s\n ",name)
-	fmt.Fprintf(w,"Address=%s\n ",address)
+	fmt.Fprintf(w,"Name= %s\n ",name)
+	fmt.Fprintf(w,"Address=  %s\n ",address)
 }
 func helloHandler(w http.ResponseWriter,r *http.Request){
 	if r.URL.Path != "/hello"{
@@ -33,7 +33,7 @@ func helloHandler(w http.ResponseWriter,r *http.Request){
 func main(){ 
 	fileServer:=http.FileServer(http.Dir("./static"))
 	http.Handle("/",fileServer)
-	http.HandleFunc("./form",formHandler)
+	http.HandleFunc("/form",formHandler)
 	http.HandleFunc("/hello",helloHandler)
 
 	fmt.Printf("Starting server at port 1941 \n")
